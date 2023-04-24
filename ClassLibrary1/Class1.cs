8979103f07ce79dabe7414a229f5fbe247af7a4d
Миргалимова Вероника Радиковна <mirgalimova.vr1638@okeit.edu>
2023-04-24 12:19:40 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int plus(int a, int b)
+        {
+            return a + b;
+        }
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("не делится на нуль!!!!!хзапзхпахзахахаах");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 calculator = new Class1();
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("a + b = {0}", calculator.plus(a, b));
+            Console.WriteLine("a - b = {0}", calculator.minus(a, b));
+            Console.WriteLine("a * b = {0}", calculator.multiply(a, b));
+            Console.WriteLine("a / b = {0}", calculator.divide(a, b));
+        }
+    }
+}
